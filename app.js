12438dd234e2094649ae2ae26f9e7048277d9302
bobbyseptianto <bobbyseptianto@gmail.com>
2020-11-12 21:40:47 +0700

    login client -> server

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require("express")
+const cors = require('cors')
+const express = require('express')
 const app = express()
 const PORT = 3000
-const cors = require("cors")
-const http = require("http").createServer(app)
-var io = require('socket.io')(http);
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 
 app.use(cors())
-
-app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('userLogin', (username) => {
+    console.log(username);
+  })
+
 });
 
 
 http.listen(PORT, () => {
-console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
