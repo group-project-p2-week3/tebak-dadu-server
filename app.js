f6f3febca7db8f807ae3744f7643da2d7d2d59b1
bobbyseptianto <bobbyseptianto@gmail.com>
2020-11-12 21:58:57 +0700

    Benerin Cors

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+let usersJoined = [];
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('userLogin', (username) => {
-    console.log(username);
+    usersJoined.push(username)
+    console.log(usersJoined);
   })
 
 });
-
 
 http.listen(PORT, () => {
   console.log('listening on *:3000');
